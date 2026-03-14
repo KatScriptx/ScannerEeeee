770273f78d4a9244dcc257c8c669bac6090a2347
GitHub Action <action@github.com>
2026-03-14 02:48:12 +0000

    Update offsets from binary

--- offsets.h
+++ offsets.h
@@ -2,3 +2,4 @@
 #define rbx_luau_load_offset 0x0 // NOT FOUND
 #define rbx_lua_pcall_offset 0x0 // NOT FOUND
 #define rbx_lua_gettop_offset 0x0 // NOT FOUND
+#define rbx_get_task_scheduler_offset 0x0 // NOT FOUND
